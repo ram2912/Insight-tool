e224c1249387106c4f33bfd9f0669919b9a0d6d3
ram2912 <124676030+ram2912@users.noreply.github.com>
2023-02-08 12:26:33 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 require('dotenv').config();
 
 const configuration = new Configuration({
-   apiKey: process.env.OPENAI_API_KEY,
+   apiKey:OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
